b325eedad45a36cd7b2e344d8df3ff86a1327a14
iamsondev <sondipkumarsk@gmail.com>
2026-03-12 00:08:51 +0600

    create login user api

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
 
 interface IRegisterPatientPayload {
@@ -21,6 +22,29 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   return data;
 };
 
+interface ILoginUser {
+  email: string;
+  password: string;
+}
+
+const loginUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+  const data = await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+    },
+  });
+  if (data.user.status === UserStatus.BLOCKED) {
+    throw new Error("User is Blocked");
+  }
+  if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+    throw new Error("User is deleted");
+  }
+  return data;
+};
+
 export const AuthService = {
   registerPatient,
+  loginUser,
 };
