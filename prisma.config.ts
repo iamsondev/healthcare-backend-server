1944007cde76b479b4bfbd5e194fc33c5631a54b
iamsondev <sondipkumarsk@gmail.com>
2026-03-11 13:30:46 +0600

    replace prisma schema and create another file for specialty schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,8 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema",
   migrations: {
     path: "prisma/migrations",
   },
