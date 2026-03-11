04d4305f4bd0a351d5ace5e0a5057169d7aaa3c3
iamsondev <sondipkumarsk@gmail.com>
2026-03-11 22:17:28 +0600

    create auth folder in the module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SpecialtyRoute } from "../modules/specialties/specialties.route";
+import { AuthRouter } from "../modules/auth/auth.route";
 
 const router = Router();
 
+router.use("/auth", AuthRouter);
 router.use("/specialties", SpecialtyRoute);
 
 export const IndexRoute = router;
