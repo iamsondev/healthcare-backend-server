2d73d1455c3609144543b14f832a5d967e09ab40
iamsondev <sondipkumarsk@gmail.com>
2026-03-11 23:42:15 +0600

    create register api for patient

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -34,7 +34,7 @@ export const auth = betterAuth({
       },
       deletedAt: {
         type: "date",
-        required: true,
+        required: false,
         defaultValue: null,
       },
     },
