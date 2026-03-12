8929706a60f7ea232975be7957299102f092eda7
iamsondev <sondipkumarsk@gmail.com>
2026-03-12 17:56:32 +0600

    create globalErrorHandler and notFound

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import { IndexRoute } from "./app/routes";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
 app.use(express.urlencoded({ extended: true }));
@@ -10,5 +12,8 @@ app.use("/app/v1/", IndexRoute);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express!");
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -20,20 +20,30 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   if (!data.user) {
     throw new Error("Failed to register patient");
   }
-  const patient = await prisma.$transaction(async (tx) => {
-    const patientTx = await tx.patient.create({
-      data: {
-        userId: data.user.id,
-        name: payload.name,
-        email: payload.email,
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      const patientTx = await tx.patient.create({
+        data: {
+          userId: data.user.id,
+          name: payload.name,
+          email: payload.email,
+        },
+      });
+      return patientTx;
+    });
+    return {
+      ...data,
+      patient,
+    };
+  } catch (error) {
+    console.log("Transaction error:", error);
+    await prisma.user.delete({
+      where: {
+        id: data.user.id,
       },
     });
-    return patientTx;
-  });
-  return {
-    ...data,
-    patient,
-  };
+    throw error;
+  }
 };
 
 interface ILoginUser {
