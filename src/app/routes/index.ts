32a851700a9d8be0bad9678818b436d02d1a2b97
iamsondev <sondipkumarsk@gmail.com>
2026-03-13 17:09:31 +0600

    create getAllDoctors api

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { SpecialtyRoute } from "../modules/specialties/specialties.route";
 import { AuthRouter } from "../modules/auth/auth.route";
 import { UserRoute } from "../modules/user/user.route";
+import { DoctorRoute } from "../modules/doctor/doctor.route";
 
 const router = Router();
 
 router.use("/auth", AuthRouter);
 router.use("/specialties", SpecialtyRoute);
 router.use("/users", UserRoute);
-
+router.use("/doctors", DoctorRoute);
 export const IndexRoute = router;
